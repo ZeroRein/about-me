2ba05bccbaa6dac806e20544187a16f8ae36f9a6
ZeroRein <131316771+ZeroRein@users.noreply.github.com>
2023-07-21 16:24:15 +0900

    7/21

--- 1/javascript/script.js
+++ 1/javascript/script.js
@@ -21,4 +21,29 @@ element.addEventListener(
 );
 });
 
+var img_src = new Array("../../img/black_00049.jpg","../../img/humberger01.webp","../../img/R.png");
+var num = 0;
+
+function change_img(){
+    if (num == 2) {
+        num = 0;
+    }
+    else {
+        num ++;
+    }
+    document.getElementById("sec-img").src=img_src[num];
+}
+change_img_timer();
+
+function change_img_timer(){
+    if (num == 2){
+        num = 0;
+    } 
+    else {
+        num ++;
+    }
+    document.getElementById("sec-img").src=img_src[num];
+    setTimeout("change_img_timer()",10000); 
+}
+
 
